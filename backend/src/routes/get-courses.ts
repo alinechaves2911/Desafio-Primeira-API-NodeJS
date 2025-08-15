bb95520c6fb8cdad66677348dd09189c5570ef3d
Aline <alinechaves9845@gmail.com>
2025-08-15 15:19:47 -0300

    Corrigindo a rota get

--- backend/src/routes/get-courses.ts
+++ backend/src/routes/get-courses.ts
@@ -5,6 +5,9 @@ import { ilike, asc, desc, SQL, and, eq, count} from 'drizzle-orm'
 import { z } from 'zod'
 
 export const  getCoursesRoute: FastifyPluginAsyncZod = async (server) =>{
+  server.get('/', async (request, reply) => {
+    return reply.redirect('/courses')
+  })
     server.get('/courses', {
         schema:{
           tags: ['courses'],
